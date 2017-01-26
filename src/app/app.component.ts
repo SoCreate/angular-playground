e46ccf3a637e7b61020a13df28017fff94c4d0fd
Justin Schwartzenberger <schwarty@gmail.com>
2017-01-25 23:09:38 -0800

    add animation, styling. remove screenshot, add link to doc site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, trigger, style, state, transition, animate} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Sandbox, SelectedSandboxAndScenarioKeys} from './shared/app-state';
 import {SANDBOXES} from './shared/tokens';
@@ -9,36 +9,49 @@ import {EventManager} from "@angular/platform-browser";
 
 @Component({
   selector: 'ap-root',
+  animations: [
+    trigger('flyInOut', [
+      state('true', style({transform: 'translate(-50%, 0)'})),
+      transition('void => *', [
+        style({transform: 'translate(-50%, -110%)'}),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({transform: 'translate(-50%, -110%)'}))
+      ])
+    ])
+  ],
   template: `
-    <div *ngIf="commandBarActive" class="command-bar">
+    <div class="command-bar-shield" *ngIf="commandBarActive" (click)="toggleCommandBar()"></div>
+    <div *ngIf="commandBarActive" class="command-bar" [@flyInOut]="commandBarActive">
       <input type="text" name="filter" placeholder="filter" [formControl]="filter">
       <div>
         <div *ngFor="let sandbox of filteredSandboxes">
           <span class="sandbox"
-                [class.selected]="selectedSandboxAndScenarioKeys?.sandboxKey === sandbox.key">
+                [class.selected]="selectedSandboxAndScenarioKeys.sandboxKey === sandbox.key">
             {{sandbox.prependText}}{{sandbox.name}}</span>
-          <div *ngFor="let scenario of sandbox.scenarios"
-               (click)="selectScenario(sandbox.key, scenario.key); toggleCommandBar()">
+          <div *ngFor="let scenario of sandbox.scenarios">
             <a class="scenario"
-               [class.selected]="selectedSandboxAndScenarioKeys?.scenarioKey === scenario.key && selectedSandboxAndScenarioKeys?.sandboxKey === sandbox.key">
+               (click)="onScenarioClick(sandbox.key, scenario.key, $event); toggleCommandBar()"
+               [class.selected]="selectedSandboxAndScenarioKeys.scenarioKey === scenario.key && selectedSandboxAndScenarioKeys.sandboxKey === sandbox.key">
               {{scenario.description}}</a>
           </div>
         </div>
       </div>
     </div>
-    <section>
-      <div *ngIf="!selectedSandboxAndScenarioKeys.sandboxKey" class="help-message">
+    <section *ngIf="!selectedSandboxAndScenarioKeys.sandboxKey" class="help-message">
+      <div>
         <template [ngIf]="totalSandboxes > 0">
           <p>The app has {{totalSandboxes}} sandboxed component{{totalSandboxes > 1 ? 's' : ''}} loaded.</p>
-          <p *ngIf="totalSandboxes > 1">Use the filter to find one to play in!</p>
-          <p *ngIf="totalSandboxes === 1">Use the filter to find it!</p>
         </template>
         <template [ngIf]="totalSandboxes === 0">
           <p>The app does not have any sandboxed components.</p>
         </template>
+        <p>Pick sandboxed components: <strong>ctrl + p</strong></p>
       </div>
-      <ap-scenario *ngIf="selectedSandboxAndScenarioKeys.sandboxKey"
-        [selectedSandboxAndScenarioKeys]="selectedSandboxAndScenarioKeys"></ap-scenario>
+    </section>
+    <section *ngIf="selectedSandboxAndScenarioKeys.sandboxKey">
+      <ap-scenario [selectedSandboxAndScenarioKeys]="selectedSandboxAndScenarioKeys"></ap-scenario>
     </section>
   `,
   styles: [`
@@ -46,31 +59,40 @@ import {EventManager} from "@angular/platform-browser";
       font-family: sans-serif;
       display: flex;
       flex-direction: column; }
-      :host .command-bar {
+      :host .command-bar-shield {
         position: absolute;
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
         width: 100%;
+        height: 100vh;
+        opacity: 0; }
+      :host .command-bar {
+        font-family: Menlo,Monaco,monospace;
+        position: absolute;
+        left: 50%;
       }
       :host .command-bar > input {
+        font-family: Menlo,Monaco,monospace;
         width: 400px;
         z-index: 1;
         padding: 4px;
         margin-top: 6px;
-        border: 1px solid cornflowerblue;
-        line-height: normal;
+        border: 1px solid #174a6c;
+        background-color: #3c3c3c;
+        font-size: 14pt;
+        color: #fff;
       }
+      :host .command-bar > input::-webkit-input-placeholder {
+        color: #a9a9a9; }
       :host .command-bar > input::-moz-focus-inner {
         border: 0;
         padding: 0; }
       :host .command-bar > div {
-        margin-top: -38px;
+        margin-top: -39px;
         width: 400px;
         padding: 34px 14px 14px 14px;
-        background-color: #3a3a3a;
+        background-color: #252526;
         color: #fff;
+        box-shadow: 0 3px 8px 5px black;
+        font-size: 14pt;
       }
       :host .command-bar > div > div:first-child {
         padding-top: 18px;
@@ -78,7 +100,7 @@ import {EventManager} from "@angular/platform-browser";
       :host .command-bar > div a {
         cursor: pointer; }
       :host .command-bar .sandbox.selected, :host .command-bar .scenario.selected {
-        font-weight: bold; }
+        color: #0097fb; }
       :host .command-bar .scenario {
         font-size: .8em;
         padding: 0 8px; }
@@ -87,6 +109,15 @@ import {EventManager} from "@angular/platform-browser";
         border: 0;
         width: 100%;
         background-color: white; }
+        
+      :host section.help-message {
+        display: flex;
+        align-items: center;
+        justify-content: center; 
+        height: 100vh; }
+      :host section.help-message > div {
+        max-width: 50%;
+        font-family: Menlo,Monaco,monospace; }
   `]
 })
 export class AppComponent {
@@ -149,8 +180,13 @@ export class AppComponent {
     this.commandBarActive = !this.commandBarActive;
   }
 
-  selectScenario(sandboxKey, scenarioKey) {
+  private selectScenario(sandboxKey, scenarioKey) {
     this.selectedSandboxAndScenarioKeys = {sandboxKey, scenarioKey};
     this.stateService.setSandboxAndScenarioKeys(this.selectedSandboxAndScenarioKeys);
+  }
+
+  onScenarioClick(sandboxKey, scenarioKey, e){
+    this.selectScenario(sandboxKey, scenarioKey);
+    e.preventDefault();
   }
 }
