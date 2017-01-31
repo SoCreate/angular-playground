a01e651c3b66f98d79d4bdf2d92ef124a47a2f76
brian treese <brian@socreate.it>
2017-01-31 09:50:12 -0800

    add styles to the info block

--- example-app/src/app/feature2/info-block.component.sandbox.ts
+++ example-app/src/app/feature2/info-block.component.sandbox.ts
@@ -1,4 +1,6 @@
 import { sandboxOf } from 'angular-playground';
 import { InfoBlockComponent } from './info-block.component';
 export default sandboxOf(InfoBlockComponent)
-  .add('with text', {template: '<ex-info-block>Here is some information</ex-info-block>'});
+  .add('A general example', {template: '<ex-info-block state="info">This is a general info block example component in Angular Playground</ex-info-block>'})
+  .add('A warning example', {template: '<ex-info-block state="warning">This is a "warning" info block example component in Angular Playground</ex-info-block>'})
+  .add('An error example', {template: '<ex-info-block state="error">This is an "error" info block example component in Angular Playground</ex-info-block>'});
