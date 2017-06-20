1e266cf2ba649dc5212f45649612b3961e55b361
Justin Schwartzenberger <schwarty@gmail.com>
2017-06-20 15:01:02 -0700

    add support for custom root module
    - remove build check for animations module
    - add PlaygroundCommonModule
    - export PlaygroundCommonModule and AppComponent
    - add MyPlaygroundModule to example cli app
    
    issue #32

---
+++ src/app/playground-common.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {CommonModule, Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ScenarioComponent} from './scenario/scenario.component';
+import {SANDBOXES} from './shared/tokens';
+import {StateService} from './shared/state.service';
+import {loadSandboxes} from './load-sandboxes';
+import {FocusDirective} from "./shared/focus.directive";
+import {UrlService} from "./shared/url.service";
+import {AppComponent} from './app.component';
+
+declare let require: any;
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    Location,
+    {provide: LocationStrategy, useClass: PathLocationStrategy},
+    {provide: SANDBOXES, useFactory: loadSandboxes},
+    StateService,
+    UrlService
+  ],
+  declarations: [AppComponent, ScenarioComponent, FocusDirective],
+  exports: [AppComponent]
+})
+export class PlaygroundCommonModule {
+}
--- src/cli/build.ts
+++ src/cli/build.ts
@@ -6,18 +6,6 @@ import * as path from 'path';
 export const build = (rootPath) => {
   let importStatements = new StringBuilder();
   let content = new StringBuilder();
-
-  let modules = [];
-  try {
-    fs.accessSync(path.resolve('node_modules/@angular/animations'));
-    importStatements.addLine(`import { BrowserAnimationsModule } from '@angular/platform-browser/animations';`);
-    modules.push('BrowserAnimationsModule');
-  } catch (e) {
-  }
-  content.addLine('');
-  content.addLine(`export const PLAYGROUND_SUPPORT_MODULES: any[] = [${modules.join(',')}];`);
-
-
   let home = path.resolve(rootPath);
   let sandboxCount = 0;
   let sandboxes = [];
