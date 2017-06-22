0352c254f016de8ba88bc715bee03dd1da185865
Justin Schwartzenberger <schwarty@gmail.com>
2017-06-22 12:25:17 -0700

    upgrade the cli to 1.1.3

--- example-app-angular-cli/src/polyfills.ts
+++ example-app-angular-cli/src/polyfills.ts
@@ -31,6 +31,7 @@
 // import 'core-js/es6/array';
 // import 'core-js/es6/regexp';
 // import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
@@ -66,3 +67,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+/**
+ * Need to import at least one locale-data with intl.
+ */
+// import 'intl/locale-data/jsonp/en';
