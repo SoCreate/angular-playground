8ac215c2cbc06eed472ebd3e9063cf1a083981ce
Jami Lurock <jamirichard@gmail.com>
2017-09-28 09:34:47 -0700

    fix examples showing how to embed sandboxes

--- examples/example-app-embed-mode/src/app/app.component.ts
+++ examples/example-app-embed-mode/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>Example App to Show Embed Mode</h1>
-    <a href="http://localhost:4201?scenario=noticecomponent/short text">View in Playground</a>
-    <iframe src="http://localhost:4201?scenario=noticecomponent/short text&embed=1" frameborder="0" width="100%"></iframe>
-    <a href="http://localhost:4201?scenario=feature1.persondetailscomponent/person with name and twitter">View in Playground</a>
-    <iframe src="http://localhost:4201?scenario=feature1.persondetailscomponent/person with name and twitter&embed=1" frameborder="0" width="100%"></iframe>
+    <a href="http://localhost:4201/?scenario=.%2Fapp%2Fshared%2Fnotice.component.sandbox/short%20text">View in Playground</a>
+    <iframe src="http://localhost:4201/?scenario=.%2Fapp%2Fshared%2Fnotice.component.sandbox/short%20text&embed=1" frameborder="0" width="100%"></iframe>
+    <a href="http://localhost:4201/?scenario=.%2Fapp%2Ffeature1%2Fperson-details.component.sandbox/person%20with%20name%20and%20twitter">View in Playground</a>
+    <iframe src="http://localhost:4201/?scenario=.%2Fapp%2Ffeature1%2Fperson-details.component.sandbox/person%20with%20name%20and%20twitter&embed=1" frameborder="0" width="100%"></iframe>
   `
 })
 export class AppComponent {}
