d87a2790d1a13d10f8d8dd6d56f7cd2e66f1648a
Graham Marlow <mgmarlow@gmail.com>
2017-10-07 11:46:02 -0700

    modify build process to use gulp

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,28 +2,15 @@ const path = require('path');
 const fs = require('fs-extra');
 const glob = require('glob');
 
-Promise.all([cleanDist(), cleanMetadata()])
+cleanDist()
     .then(() => console.log('Cleaned build.'))
     .catch((err) => console.log(err));
 
 ////////////////////////////////////////////
 
 function cleanDist() {
-    return fs.remove(path.join(__dirname, '../dist/'));
-}
-
-function cleanMetadata() {
-    const metadata = [
-        path.join(__dirname, '../src/**/*.ngfactory.ts'),
-        path.join(__dirname, '../src/**/*.ngsummary.json')
-    ];
-
-    let promises = [];
-    metadata.forEach(path => {
-        const patternMatches = glob(path, (err, files) => {
-            files.forEach(file => promises.push(fs.removeSync(file)));
-        });
-    });
-
-    return Promise.all(promises);
+    return Promise.all([
+        fs.remove(path.join(__dirname, '../dist/')),
+        fs.remove(path.join(__dirname, '../build/')),
+    ]);
 }
