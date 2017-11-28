3fedd9c47f26415d59469e375e353e0b52b554e5
mgmarlow <Graham.Marlow@socreate.it>
2017-11-28 12:48:19 -0800

    fix indentation

--- src/cli/start-watch.ts
+++ src/cli/start-watch.ts
@@ -2,12 +2,12 @@ const path = require('path');
 const watch = require('node-watch');
 
 export const startWatch = (config, cb) => {
-  let filter = (fn) => {
-    return (filename) => {
-      if (!/node_modules/.test(filename) && /\.sandbox.ts$/.test(filename)) {
-        fn(filename);
-      }
+    let filter = (fn) => {
+        return (filename) => {
+            if (!/node_modules/.test(filename) && /\.sandbox.ts$/.test(filename)) {
+                fn(filename);
+            }
+        };
     };
-  };
-  watch([path.resolve(config.sourceRoot)], filter(cb));
+    watch([path.resolve(config.sourceRoot)], filter(cb));
 };
