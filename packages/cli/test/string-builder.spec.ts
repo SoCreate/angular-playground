68ff2b569489d815a465665ace5a85228aae4f9e
Graham Marlow <mgmarlow@gmail.com>
2018-01-07 14:41:42 -0800

    add string builder tests

--- packages/cli/test/string-builder.spec.ts
+++ packages/cli/test/string-builder.spec.ts
@@ -1,3 +1,15 @@
-it('should append strings to newlines', () => {
-  expect(true).toBeTruthy();
+import { StringBuilder } from '../lib/string-builder';
+
+it('should append newlines to each added line', () => {
+    const builder = new StringBuilder();
+    builder.addLine('test');
+    builder.addLine('test2');
+    expect(builder.dump()).toBe('test\ntest2\n');
+});
+
+it('should clear out internal data structure after dumping', () => {
+    const builder = new StringBuilder();
+    builder.addLine('test');
+    builder.dump();
+    expect(builder.dump()).toBe('\n');
 });
