419dd88f0b4d966ae46d2f414d97f9e3cc553e4a
mgmarlow <Graham.Marlow@socreate.it>
2018-03-05 15:09:43 -0800

    add error-reporter tests

---
+++ packages/angular-playground/cli/test/error-reporter.spec.ts
@@ -0,0 +1,49 @@
+import {} from 'jest';
+import fs = require('fs');
+import chalk from 'chalk';
+import { ErrorReporter, REPORT_TYPE } from '../src/error-reporter';
+
+describe('ErrorReporter', () => {
+    const mockScenarios = [{
+        url: 'http://localhost:4201/textbox',
+        name: 'textbox',
+        description: 'Default'
+    }];
+    let reporter;
+
+    beforeEach(() => {
+        reporter = new ErrorReporter(mockScenarios, 'output.json', REPORT_TYPE.LOG);
+    });
+
+    it('should initialize with no errors', () => {
+        expect(reporter.errors.length).toBe(0);
+    });
+
+    it('should add descriptions and scenarios separately to errors array', () => {
+        reporter.addError('LOG Error', 'http://localhost:4201/textbox');
+        expect(reporter.errors.length).toBe(1);
+        expect(reporter.errors).toEqual([{
+            descriptions: 'LOG Error',
+            scenario: 'http://localhost:4201/textbox'
+        }]);
+    });
+
+    it('should console.log on compileReport if report type is log', () => {
+        const errorSpy = spyOn(console, 'error');
+        const consoleSpy = spyOn(console, 'log');
+        reporter.addError('LOG Error', 'http://localhost:4201/textbox');
+        reporter.addError('Read Error', 'http://localhost:4201/textbox');
+        reporter.compileReport();
+        expect(errorSpy).toHaveBeenCalledWith(chalk.red('Error in the following scenarios'));
+        expect(consoleSpy).toHaveBeenCalledTimes(4);
+    });
+
+    it('should write to file if report type is json', () => {
+        const writeFileSpy = spyOn(fs, 'writeFileSync');
+        reporter.type = REPORT_TYPE.JSON;
+        reporter.addError('LOG Error', 'http://localhost:4201/textbox');
+        reporter.addError('Read Error', 'http://localhost:4201/textbox');
+        reporter.compileReport();
+        expect(writeFileSpy).toHaveBeenCalled();
+    });
+});
