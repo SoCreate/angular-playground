2e866dc1194b83860b5b49cd17009b38d38dc0f8
mgmarlow <Graham.Marlow@socreate.it>
2018-03-13 16:28:48 -0700

    use object assign over initial value instead of replacing value

--- packages/angular-playground/core/src/playground.module.ts
+++ packages/angular-playground/core/src/playground.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PlaygroundCommonModule } from './playground-common.module';
 import { Middleware, MIDDLEWARE } from '../lib/middlewares';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { initializePlayground } from '../lib/initialize-playground';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 declare let require: any;
 
 const _middleware = new BehaviorSubject<Middleware>({
     selector: null,
-    modules: [],
-    overlay: false
+    overlay: false,
+    modules: []
 });
 const middleware = _middleware.asObservable();
 
@@ -28,12 +28,10 @@ const middleware = _middleware.asObservable();
 export class PlaygroundModule {
     static configure(configuration: Middleware) {
         if (!configuration.selector) {
-            throw new Error('Please provide the selector for the application you would like Playground to bootstrap.');
+            throw new Error('Please provide a valid selector for `PlaygroundModule.configure()`.');
         }
-
         initializePlayground(configuration.selector);
-        _middleware.next(configuration);
-
+        _middleware.next({ ..._middleware.value, ...configuration });
         return this;
     }
 }
