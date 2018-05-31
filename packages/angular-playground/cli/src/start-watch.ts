cb69545bebdfb94d2467acf77070c63dc8818f86
Jami Lurock <jamirichard@gmail.com>
2018-05-31 15:13:20 -0700

    fix issue with sandbox files not being compiled properly when added or removed

--- packages/angular-playground/cli/src/start-watch.ts
+++ packages/angular-playground/cli/src/start-watch.ts
@@ -3,12 +3,12 @@ import watch = require('node-watch');
 
 export function startWatch(sourceRoot: string, cb: Function) {
     const filter = (fn: Function) => {
-        return (filename: string) => {
+        return (evt: string, filename: string) => {
             if (!/node_modules/.test(filename) && /\.sandbox.ts$/.test(filename)) {
                 fn(filename);
             }
         };
     };
 
-    watch([resolvePath(sourceRoot)], filter(cb));
+    watch([resolvePath(sourceRoot)], { recursive: true }, filter(cb));
 }
