adc7dece539d7826f759f1cde0a31cea3823215a
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2018-05-31 10:45:54 +0200

    add cli schematics #73

---
+++ packages/angular-playground/schematics/src/utils/npm-script.ts
@@ -0,0 +1,26 @@
+import { Tree } from '@angular-devkit/schematics';
+
+/**
+ * Adds a script to the package.json
+ */
+export function addNpmScriptToPackageJson(
+  host: Tree,
+  name: string,
+  command: string
+): Tree {
+  if (host.exists('package.json')) {
+    const sourceText = host.read('package.json')!.toString('utf-8');
+    const json = JSON.parse(sourceText);
+    if (!json.scripts) {
+      json.scripts = {};
+    }
+
+    if (!json.scripts[name]) {
+      json.scripts[name] = command;
+    }
+
+    host.overwrite('package.json', JSON.stringify(json, null, 2));
+  }
+
+  return host;
+}
