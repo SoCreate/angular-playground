386b94d95f1495cf40b0b7338a0aad2bbc906436
Timothy Stoddard <tim.stoddard2@gmail.com>
2018-08-23 10:00:10 -0700

    fix relative import of sandboxed component when using the sandbox generate command

--- packages/angular-playground/schematics/src/sandbox/files/__name@dasherize__.component.sandbox.ts
+++ packages/angular-playground/schematics/src/sandbox/files/__name@dasherize__.component.sandbox.ts
@@ -1,5 +1,5 @@
 import { sandboxOf } from 'angular-playground';
-import { <%= classify(name) %>Component } from './<%= name %>.component';
+import { <%= classify(name) %>Component } from '<%= flat ? '.' : '..' %>/<%= name %>.component';
 
 export default sandboxOf(<%= classify(name) %>Component)
   .add('default', {
