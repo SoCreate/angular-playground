758bf1518f4d55d9236059b91d6377432e046632
Timothy Stoddard <tim.stoddard2@gmail.com>
2019-03-19 15:51:54 -0700

    add new error reporter for junit xml format

---
+++ packages/angular-playground/cli/src/check-errors/reporters/xml-reporter.ts
@@ -0,0 +1,34 @@
+import { ErrorReport, Reporter } from '../../error-reporter';
+import { ScenarioSummary } from '../verify-sandboxes';
+
+export class XMLReporter implements Reporter {
+    constructor (
+        public errors: ErrorReport[],
+        public scenarios: ScenarioSummary[]
+    ) {}
+
+    getReport() {
+        let result = `<testsuite name="Check Sandbox Errors" tests="${this.scenarios.length}" failures="${this.errors.length}" skipped="0">\n`;
+        for (const scenario of this.scenarios) {
+            result += `<testcase classname="${scenario.name}" name="${scenario.description}">\n`;
+            const failure = this.errors.find(error =>
+                error.scenario === scenario.name && error.scenarioTitle === scenario.description);
+            if (failure) {
+                const message = this.htmlEncode(failure.descriptions[0]);
+                result += `<failure message="Scenario Failed">${message}</failure>\n`;
+            }
+            result += '</testcase>\n';
+        }
+        result += '</testsuite>\n';
+        return result;
+    }
+
+    private htmlEncode(s: string) {
+        return s
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    }
+}
