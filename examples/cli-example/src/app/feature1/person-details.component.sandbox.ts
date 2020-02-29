c49e4ba85ed6407d7a8642246f8862c7d213871e
Jami Lurock <Jami.Lurock@SoCreate.it>
2020-02-28 16:01:51 -0800

    update example project with new updated settings

--- examples/cli-example/src/app/feature1/person-details.component.sandbox.ts
+++ examples/cli-example/src/app/feature1/person-details.component.sandbox.ts
@@ -1,13 +1,10 @@
 import { sandboxOf } from 'angular-playground';
 import { PersonDetailsComponent } from './person-details.component';
 import { PersonBioComponent } from './person-bio.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 export default sandboxOf(PersonDetailsComponent, {
   label:'feature1',
-  declarations: [PersonBioComponent],
-  entryComponents: [PersonBioComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+  declarations: [PersonBioComponent]
 })
   .add('person with name and twitter', {
     template: '<ex-person-details [person]="person"></ex-person-details>',
