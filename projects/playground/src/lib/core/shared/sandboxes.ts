b3c6b63a9923c27955a197fb0fb5aa4b21f644d3
Jami Lurock <Jami.Lurock@SoCreate.it>
2021-05-21 08:32:39 -0700

    fix typing issues

--- projects/playground/src/lib/core/shared/sandboxes.ts
+++ projects/playground/src/lib/core/shared/sandboxes.ts
@@ -2,11 +2,11 @@
 // Empty functions are the expected initial value.
 
 export abstract class Sandboxes {
-    getSandbox(path: string) {
+    getSandbox(path: string): any {
         /*GET_SANDBOX*/
         /*END_GET_SANDBOX*/
     }
-    getSandboxMenuItems() {
+    getSandboxMenuItems(): any {
         /*GET_SANDBOX_MENU_ITEMS*/
         return [];
         /*END_GET_SANDBOX_MENU_ITEMS*/
